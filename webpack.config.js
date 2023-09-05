f04b8e35911db8e005c6e0c2fe40c61dfdc2a55d
Joynie12 <bigw199412@gmail.com>
2023-09-05 22:16:29 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './index.js', 
+    entry: './src/index.js', 
     output: {
         path: path.resolve(__dirname, 'dist'), 
         filename: 'bundle.js', 
